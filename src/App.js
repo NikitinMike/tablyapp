a558f78e8f22ff3735337a952d80bd04945e71ed
mike <jobles11@gmail.com>
2022-01-28 23:53:26 +0300

    make on click handler

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import './App.css'
 
-const table = {
+const tableContent = {
     headers: ['№', 'Фамилия', 'Имя', 'Отчество', 'Год', 'Адрес', 'Паспорт'], rows: [
+        [],
         ['', 'jsdidhx', 'ghdhdhd', 'jfjfdjdfdsl', 356778, 'jdiow euhu wsuh', 'hdjfjikshf'],
         ['', 'uijflsdjh', 'ffffvc', 'ehjjkjf', 6899],
         ['', 'idjskf', 'vbccfff', 'dhjgfd', 57890],
@@ -12,10 +13,10 @@ const table = {
 
 const App = () =>
     <div className='App'>
-        <Tab cols="5" table={table}/>
+        <Table table={tableContent} cols="5"/>
     </div>
 
-const Head = ({headers, cols}) =>
+const Header = ({headers, cols}) =>
     <thead>
     <tr>
         {headers.slice(0, cols).map((header, index) =>
@@ -23,14 +24,20 @@ const Head = ({headers, cols}) =>
     </tr>
     </thead>
 
-const Data = ({text}) => <td>{text}</td>
+const Data = ({text, id}) =>
+    <td key={id} onClick={(e) => consoleLog(id, e.button)}>{text}</td>
+
+function consoleLog(text) {
+    console.log(text)
+}
 
 const Body = ({rows, cols}) =>
     <tbody>
-    {rows.map((row, index) =>
-        <tr key={index}>
-            <td>{index}</td>
-            {row.slice(1, cols).map((text, index) => <Data key={index} text={text}/>)}
+    {rows.map((row, rowIndex) => !rowIndex ? null
+        : <tr key={rowIndex}>
+            <td>{rowIndex}</td>
+            {row.slice(1, cols).map((text, colIndex) =>
+                <Data id={rowIndex * 10 + colIndex + 1} key={colIndex} text={text}/>)}
         </tr>
     )}
     </tbody>
@@ -44,10 +51,10 @@ const Footer = ({cols, footers}) =>
     </tr>
     </tfoot>
 
-const Tab = ({cols, table}) =>
+const Table = ({cols, table}) =>
     <div>
         <table rules='all' frame='border'>
-            <Head headers={table.headers} cols={cols}/>
+            <Header headers={table.headers} cols={cols}/>
             <Body rows={table.rows} cols={cols}/>
             <Footer cols={cols} footers={table.footers}/>
         </table>
