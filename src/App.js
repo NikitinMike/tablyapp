05f45f7059143d65478a85478572dab90f865f85
mike <jobles11@gmail.com>
2022-03-14 14:57:27 +0300

    update Table Row class

--- src/App.js
+++ src/App.js
@@ -5,57 +5,57 @@ const headers = ['№', 'Фамилия', 'Имя', 'Отчество', 'Год'
 const footers = ['', '111', '222', 'sum', '', '', '']
 
 const Header = ({headers, cols}) =>
-  <thead>
-  <tr onClick={(e) => addRow({})}>
-    {headers.slice(0, cols).map((header, index) =>
-      <th key={index}>{header ? header : index}</th>)}
-  </tr>
-  </thead>
+    <thead>
+    <tr onClick={(e) => addRow({})}>
+        {headers.slice(0, cols).map((header, index) =>
+            <th key={index}>{header ? header : index}</th>)}
+    </tr>
+    </thead>
 
 const Footer = ({cols, footers}) =>
-  <tfoot>
-  <tr onClick={(e) => addRow({})}>
-    {footers.slice(0, cols).map(
-      (text, index) => <td key={index}>{text ? text : index}</td>
-    )}
-  </tr>
-  </tfoot>
+    <tfoot>
+    <tr onClick={(e) => addRow({})}>
+        {footers.slice(0, cols).map(
+            (text, index) => <td key={index}>{text ? text : index}</td>
+        )}
+    </tr>
+    </tfoot>
 
 const Table = ({cols, table, caption}) =>
-  <div>
-    <table rules='all' frame='border'>
-      <caption>{caption}</caption>
-      <Header headers={table.headers} cols={cols}/>
-      <Body rows={table.rows} cols={cols}/>
-      <Footer cols={cols} footers={table.footers}/>
-    </table>
-  </div>
+    <div>
+        <table rules='all' frame='border'>
+            <caption>{caption}</caption>
+            <Header headers={table.headers} cols={cols}/>
+            <Body rows={table.rows} cols={cols}/>
+            <Footer cols={cols} footers={table.footers}/>
+        </table>
+    </div>
 
 async function getRow(id) {
-  await fetch('http://localhost:3000/contacts/' + id, {method: 'GET'})
-    .then(response => response.json())
-    .then(async data => {
-      console.log(data)
-    })
-    .catch(e => console.log(e))
+    await fetch('http://localhost:3000/contacts/' + id, {method: 'GET'})
+        .then(response => response.json())
+        .then(async data => {
+            console.log(data)
+        })
+        .catch(e => console.log(e))
 }
 
 async function addRow(data) {
-  await fetch('http://localhost:3000/contacts/',
-    {
-      headers: {
-        // 'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      method: 'POST',
-      body: JSON.stringify(data)
-    })
-    .then(response => response.json())
-    .then(res => {
-      console.log(res)
-    })
-    .catch(e => console.log(e))
-  window.location.reload();
+    await fetch('http://localhost:3000/contacts/',
+        {
+            headers: {
+                // 'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify(data)
+        })
+        .then(response => response.json())
+        .then(res => {
+            console.log(res)
+        })
+        .catch(e => console.log(e))
+    window.location.reload();
 }
 
 const Input = () => <input hidden={true}/>
@@ -63,84 +63,85 @@ const Input = () => <input hidden={true}/>
 
 const Data = ({col, text, row}) =>
     <td onClick={(e) => console.log(e, col, text, row)}>
-      {/*<Input />*/}
-      {text}
+        {/*<Input />*/}
+        {text}
     </td>
 
-const Body = ({rows, cols}) =>
-  <tbody>
-  {rows.map((row, rowIndex) =>
+const Row = ({row, rowIndex}) =>
     <tr key={rowIndex}>
-      <td key={row.id} onClick={(e) => addRow(row)}>{row.id}</td>
-      <Data col={'firstName'} text={row.firstName} row={row.id}/>
-      <Data col={'lastName'} text={row.lastName} row={row.id}/>
-      <Data col={'email'} text={row.email} row={row.id}/>
-      <Data col={'phone'} text={row.phone} row={row.id}/>
-      <Data col={'city'} text={row.city} row={row.id}/>
-      <Data col={'country'} text={row.country} row={row.id}/>
-      <td onClick={(e) => editRow(e, row.id)}>[...]</td>
-      <td onClick={(e) => deleteRow(e, row.id)}>[X]</td>
+        <td key={row.id} onClick={(e) => addRow(row)}>{row.id}</td>
+        <Data col={'firstName'} text={row.firstName} row={row.id}/>
+        <Data col={'lastName'} text={row.lastName} row={row.id}/>
+        <Data col={'email'} text={row.email} row={row.id}/>
+        <Data col={'phone'} text={row.phone} row={row.id}/>
+        <Data col={'city'} text={row.city} row={row.id}/>
+        <Data col={'country'} text={row.country} row={row.id}/>
+        <td onClick={(e) => editRow(e, row.id)}>[...]</td>
+        <td onClick={(e) => deleteRow(e, row.id)}>[X]</td>
     </tr>
-  )}
-  </tbody>
+
+const Body = ({rows, cols}) =>
+    <tbody>
+        {rows.map((row, rowIndex) => <Row row={row} rowIndex={rowIndex}/>)}
+    </tbody>
 
 function editRow(e, row) {
-  console.log(row)
-  const td = e.target.lastChild
-  // const td = this.props.children
-  // const td = React.Children.toArray()
-  // e.target.style.background = e.target.style.background ? '' : 'RED'
-  // if(td && td.localName==='input') td.hidden=false
-  console.log(td)
-  // td = <input/>
-  // td.focused=true
+    console.log(row)
+    const td = e.target.lastChild
+    // const td = this.props.children
+    // const td = React.Children.toArray()
+    // e.target.style.background = e.target.style.background ? '' : 'RED'
+    // if(td && td.localName==='input') td.hidden=false
+    console.log(td)
+    // td = <input/>
+    // td.focused=true
 }
 
 function deleteRow(e, row) {
-  console.log(row)
-  fetch('http://localhost:3000/contacts/' + row, {method: 'DELETE'})
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(e => console.log(e))
-  window.location.reload();
+    console.log(row)
+    fetch('http://localhost:3000/contacts/' + row, {method: 'DELETE'})
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(e => console.log(e))
+    window.location.reload();
 }
 
 function consoleLog(id, e) {
-  console.log(e.target)
+    console.log(e.target)
 }
 
 class App extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      rows: []
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            rows: []
+        };
+    }
 
-  render() {
-    const rows = this.state.rows
-    return (
-      <div className='App'>
-        <form>
-          <Table table={{headers, rows, footers}} cols="7" caption="C O N T A C T S"/>
-        </form>
-      </div>
-    )
-  }
+    render() {
+        const rows = this.state.rows
+        return (
+            <div className='App'>
+                <form>
+                    <Table table={{headers, rows, footers}} cols="7" caption="C O N T A C T S"/>
+                </form>
+            </div>
+        )
+    }
 
-  componentDidMount() {
-    this.getData();
-  }
+    componentDidMount() {
+        this.getData();
+    }
 
-  getData() {
-    fetch('http://localhost:3000/contacts')
-      .then(response => response.json())
-      .then(data => this.setState({rows: data, isLoaded: true}))
-      .catch(e => console.log(e))
-  }
+    getData() {
+        fetch('http://localhost:3000/contacts')
+            .then(response => response.json())
+            .then(data => this.setState({rows: data, isLoaded: true}))
+            .catch(e => console.log(e))
+    }
 }
 
 export default App
