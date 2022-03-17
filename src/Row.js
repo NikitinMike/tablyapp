0439b3dc892514f8a8631df5f3790dfd4e159e98
mike <jobles11@gmail.com>
2022-03-17 08:59:55 +0300

    implement data functions to class methods

--- src/Row.js
+++ src/Row.js
@@ -2,37 +2,37 @@ import React from 'react'
 import Fields from './Fields'
 import './App.css'
 
-async function addRow(data) {
-    await fetch('http://localhost:3000/contacts/',
-        {
-            headers: {
-                // 'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            method: 'POST',
-            body: JSON.stringify(data)
-        })
-        .then(response => response.json())
-        .then(res => {
-            console.log(res)
-        })
-        .catch(e => console.log(e))
-    window.location.reload();
-}
-
-function deleteRow(e, row) {
-    console.log(row)
-    fetch('http://localhost:3000/contacts/' + row, {method: 'DELETE'})
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(e => console.log(e))
-    window.location.reload();
-}
-
 class Row extends React.Component {
 
     row = null
     index = 0
+
+    async addRow(data) {
+        await fetch('http://localhost:3000/contacts/',
+            {
+                headers: {
+                    // 'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                method: 'POST',
+                body: JSON.stringify(data)
+            })
+            .then(response => response.json())
+            .then(res => {
+                console.log(res)
+            })
+            .catch(e => console.log(e))
+        window.location.reload();
+    }
+
+    deleteRow(e, row) {
+        console.log(row)
+        fetch('http://localhost:3000/contacts/' + row, {method: 'DELETE'})
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(e => console.log(e))
+        window.location.reload();
+    }
 
     data(col, row) {
         return (
@@ -53,7 +53,7 @@ class Row extends React.Component {
         // console.log(this.state)
     }
 
-    select(e,id) {
+    select(e, id) {
         // console.log("ROW",e.target.parentElement)
         this.index = id
         // this.index = e.target.parentElement.childNodes[0];
@@ -65,17 +65,17 @@ class Row extends React.Component {
         const row = this.state.row
         const id = row.id;
         return (
-            <tr key={this.state.index} onClick={(e) => this.props.select(e,id)}>
-                <td onClick={(e) => addRow(row)}>{id}</td>
+            <tr key={this.state.index} onClick={(e) => this.props.select(e, id)}>
+                <td onClick={(e) => this.addRow(row)}>{id}</td>
                 {/*{Fields.forEach(field => {this.data(field,row)} )}*/}
-                {this.data(Fields[0],row)}
-                {this.data(Fields[1],row)}
-                {this.data(Fields[2],row)}
-                {this.data(Fields[3],row)}
-                {this.data(Fields[4],row)}
-                {this.data(Fields[5],row)}
+                {this.data(Fields[0], row)}
+                {this.data(Fields[1], row)}
+                {this.data(Fields[2], row)}
+                {this.data(Fields[3], row)}
+                {this.data(Fields[4], row)}
+                {this.data(Fields[5], row)}
                 {/*<td onClick={(e) => editRow(e, row.id)}>[...]</td>*/}
-                <td onClick={(e) => deleteRow(e, id)}>[X]</td>
+                <td onClick={(e) => this.deleteRow(e, id)}>[X]</td>
             </tr>
         )
     }
