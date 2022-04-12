ca9ec5b6ac7bc3a3a09e120c54fbe73a36fea911
mike <jobles11@gmail.com>
2022-04-12 11:27:38 +0300

    moving tags

--- src/Fields.js
+++ src/Fields.js
@@ -1,4 +1,4 @@
-export const Headers = ['№', 'Фамилия', 'Имя', 'Отчество', 'Год', 'Адрес', 'Паспорт']
-export const Footers = ['', '111', '222', 'sum', '', '', '']
+export const Headers = ['№', 'Фамилия', 'Имя', 'Отчество', 'Год', 'Адрес', 'Паспорт', '^^^']
+export const Footers = ['', '111', '222', 'sum', '', '', '','vvv']
 const Fields = ['firstName', 'lastName', 'email', 'phone', 'city', 'country']
 export default Fields
