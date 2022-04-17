f293ef68f605b7c2852cf267d9fc7f2c8eb22363
mike <jobles11@gmail.com>
2022-04-17 10:12:37 +0300

    release tag form

--- src/App.js
+++ src/App.js
@@ -8,11 +8,10 @@ class App extends React.Component {
   render() {
     return (
       <div className='App'>
-        <form>
-          <Table cols="7" caption=" C O N T A C T S "/>
-        </form>
+        <Table cols="7" caption=" C O N T A C T S "/>
       </div>
     )
   }
 }
+
 export default App
