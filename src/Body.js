6b5911a9e5da7f7b1c33f29a0799798998765598
mike <jobles11@gmail.com>
2022-04-21 22:21:04 +0300

    body table clean

--- src/Body.js
+++ src/Body.js
@@ -5,33 +5,30 @@ import EditRow from './EditRow'
 
 class Body extends React.Component {
 
-  index = 0
-
   constructor(props) {
     super(props);
-    this.state = {table: props.table, index: this.index};
+    this.table = props.table
+    this.state = {index: 0};
     this.select = this.select.bind(this);
   }
 
   select(index) {
-    this.index = index
-    this.setState({index: this.index})
+    this.setState({index: index})
   }
 
   getRow(row, index) {
-    return ((this.state.index === row.id)
-        ? <EditRow key={row.id} row={row} index={index}/>
+    return row.id === this.state.index
+      ? <EditRow key={row.id} row={row} index={index}/>
         : <Row key={index} row={row} index={index}
                select={this.select} getPage={this.props.getData}/>
-    )
   }
 
-  mapRows() {
-    return (this.state.table.map((row, index) => this.getRow(row, index)))
+  mapRows(table) {
+    return (table.map((row, index) => this.getRow(row, index)))
   }
 
   render() {
-    return (<tbody>{this.mapRows()}</tbody>)
+    return (<tbody>{this.mapRows(this.table)}</tbody>)
   }
 }
 
