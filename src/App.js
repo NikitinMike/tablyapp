a9f03069aedc9d609930a3ec38c6c7c836b4d689
mike <jobles11@gmail.com>
2022-04-22 14:31:34 +0300

    remove ()

--- src/App.js
+++ src/App.js
@@ -6,11 +6,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
   render() {
-    return (
-      <div className='App'>
-        <Table cols="7" rows="7" caption=" C O N T A C T S "/>
-      </div>
-    )
+    return <div className='App'>
+      <Table cols="7" rows="7" caption=" C O N T A C T S "/>
+    </div>
   }
 }
 
