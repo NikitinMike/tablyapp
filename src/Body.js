fcf12c8bbc082eefc3d18a89853f5469a210e009
mike <jobles11@gmail.com>
2022-04-24 11:35:33 +0300

    render show / edit

--- src/Body.js
+++ src/Body.js
@@ -9,18 +9,12 @@ class Body extends React.Component {
     super(props);
     this.table = props.table
     this.state = {index: null};
-    this.select = this.select.bind(this);
-  }
-
-  select(index) {
-    this.setState({index: index})
   }
 
   getRow(row, index) {
-    return this.state.index === index
-      ? <RowEdit key={row.id} row={row} index={index}/>
-        : <Row key={index} row={row} index={index}
-               select={this.select} getPage={this.props.getData}/>
+    // return this.state.index === index
+    //   ? <RowEdit key={row.id} row={row} index={index}/>
+    return <Row key={index} row={row} index={index} getPage={this.props.getData}/>
   }
 
   render() {
--- src/Row.js
+++ src/Row.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './App.css'
 import {addRow, deleteRow} from "./DataService";
+import {getRow, putRow} from "./DataService";
 
 class Row extends React.Component {
 
@@ -8,7 +9,19 @@ class Row extends React.Component {
     super(props);
     this.index = props.index
     this.row = props.row
+    this.state = {edit: false};
     this.fields = Object.keys(props.row).slice(1)
+    this.select = this.select.bind(this);
+  }
+
+  select(index) {
+    console.log(index)
+    this.setState({index: index})
+  }
+
+  componentWillUnmount() {
+    // console.log(this.state,this.index,this.row)
+    if (this.state.edit) putRow(this.row).then(r => console.log(r))
   }
 
   addRow(row) {
@@ -24,7 +37,7 @@ class Row extends React.Component {
   }
 
   show() {
-    return <tr key={this.index} onClick={() => this.props.select(this.index)}>
+    return <tr key={this.index} onClick={() => this.select(this.index)}>
       <td onClick={() => this.addRow({...this.row})}>
         <div>{this.index}:{this.row.id}</div>
       </td>
@@ -35,8 +48,40 @@ class Row extends React.Component {
     </tr>
   }
 
+  editData(e, row, col) {
+    const td = e.target // .lastChild
+    // e.target.style.background = e.target.style.background ? '' : 'RED'
+    if (td.value === row[col]) return
+    row[col] = td.value
+    this.row = row
+    this.setState({edit: true})
+  }
+
+  input(col, row) {
+    return <td key={col + row.id}>
+      <input value={row[col]} color={'RED'} size={12}
+             onChange={(e) => this.editData(e, row, col)}/>
+    </td>
+  }
+
+  async getRow(id) {
+    this.row = await getRow(id)
+    this.setState({edit: false});
+  }
+
+  edit() {
+    return <tr key={this.index}>
+      <td id={this.index}>{this.row.id}</td>
+      {this.fields.map(field => this.input(field, this.row))}
+      <td onClick={() => this.getRow(this.row.id).then(() => console.log(this.row))}
+          hidden={!this.state.edit}>
+        [ xxx ]
+      </td>
+    </tr>
+  }
+
   render() {
-    return this.show()
+    return true ?this.show(): this.edit()
   }
 }
 
