14e8eb6a08604f7d5328b0d42488a414a05ef7ff
mike <jobles11@gmail.com>
2022-05-06 12:13:58 +0300

    set user context

--- src/App.js
+++ src/App.js
@@ -1,55 +1,29 @@
 import React from 'react'
 import Page from './Page'
 import './App.css'
-import {UserContext, ThemeContext, AuthContext} from "./DataService";
+import {UserContext, ThemeContext, AuthContext, DataService} from "./DataService";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as PropTypes from "prop-types";
 
 class App extends React.Component {
+
+  // async componentDidMount() {
+  //   const token = await DataService.tokenGet('john', 'changeme')
+  //   console.log(token)
+  // }
+
   render() {
     const {signedInUser, theme} = this.props;
     return <div className='App'>
       <ThemeContext.Provider value={theme}>
         <UserContext.Provider value={signedInUser}>
           <AuthContext.Provider value={undefined}>
-            <Layout/>
             <Page cols="7" rows="10" caption=" C O N T A C T S "/>
           </AuthContext.Provider>
         </UserContext.Provider>
       </ThemeContext.Provider>
     </div>
   }
-}
-
-function Sidebar() {
-  return null;
-}
-
-function Layout() {
-  return <div>
-    <Sidebar/>
-    <Content/>
-  </div>
-}
-
-function ProfilePage(props) {
-  return null;
-}
-
-ProfilePage.propTypes = {
-  theme: PropTypes.string,
-  user: PropTypes.shape({name: PropTypes.string})
-};
-
-// Компонент, который может использовать несколько контекстов
-function Content() {
-  return <ThemeContext.Consumer>
-    {theme => (
-      <UserContext.Consumer>
-        {user => (<ProfilePage name={user} theme={theme}/>)}
-      </UserContext.Consumer>
-    )}
-  </ThemeContext.Consumer>
 }
 
 export default App
